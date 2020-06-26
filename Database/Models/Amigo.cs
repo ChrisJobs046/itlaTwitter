41c252dbdb1a21ff3233e78b63d40c4a74ed1c0a
ChrisJobs046 <ChrisJobs046@gmail.com>
2020-06-26 02:29:51 -0400

    Ya

---
+++ Database/Models/Amigo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Database.Models
+{
+    public partial class Amigo
+    {
+        public int IdUsuario { get; set; }
+        public int NUsuario1 { get; set; }
+
+        public virtual Usuario IdUsuarioNavigation { get; set; }
+        public virtual Usuario NUsuario1Navigation { get; set; }
+    }
+}
